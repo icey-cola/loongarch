3daa9730a5a5de007d35a51c76608caf4c6b0af8
3022232066-Wangjiarui <574583469@qq.com>
2024-10-22 20:07:51 +0800

    移位测试未完成

--- TEMU/temu/include/cpu/r-type.h
+++ TEMU/temu/include/cpu/r-type.h
@@ -2,5 +2,14 @@
 #define __RTYPE_H__
 
 make_helper(and);
+make_helper(nor);
+make_helper(or);
+make_helper(xor);
+make_helper(sllv);
+make_helper(sll);
+make_helper(srav);
+make_helper(sra);
+make_helper(srlv);
+make_helper(srl);
 
 #endif
--- TEMU/temu/src/cpu/exec.c
+++ TEMU/temu/src/cpu/exec.c
@@ -1,5 +1,6 @@
 #include "helper.h"
 #include "all-instr.h"
+#include "monitor.h"
 
 typedef void (*op_fun)(uint32_t);
 static make_helper(_2byte_esc);
@@ -14,7 +15,7 @@ op_fun opcode_table [64] = {
 /* 0x00 */	_2byte_esc, inv, inv, inv,
 /* 0x04 */	inv, inv, inv, inv,
 /* 0x08 */	inv, inv, inv, inv,
-/* 0x0c */	inv, ori, inv, lui,
+/* 0x0c */	andi,ori, xori, lui,
 /* 0x10 */	inv, inv, temu_trap, inv,
 /* 0x14 */	inv, inv, inv, inv,
 /* 0x18 */	inv, inv, inv, inv,
@@ -30,8 +31,8 @@ op_fun opcode_table [64] = {
 };
 
 op_fun _2byte_opcode_table [64] = {
-/* 0x00 */	inv, inv, inv, inv, 
-/* 0x04 */	inv, inv, inv, inv, 
+/* 0x00 */	sll, inv, srl, sra, 
+/* 0x04 */	sllv, inv, srlv, srav, 
 /* 0x08 */	inv, inv, inv, inv, 
 /* 0x0c */	inv, inv, inv, inv, 
 /* 0x10 */	inv, inv, inv, inv, 
@@ -39,7 +40,7 @@ op_fun _2byte_opcode_table [64] = {
 /* 0x18 */	inv, inv, inv, inv, 
 /* 0x1c */	inv, inv, inv, inv, 
 /* 0x20 */	inv, inv, inv, inv, 
-/* 0x24 */	and, inv, inv, inv,
+/* 0x24 */	and, or, xor, nor,
 /* 0x28 */	inv, inv, inv, inv, 
 /* 0x2c */	inv, inv, inv, inv, 
 /* 0x30 */	inv, inv, inv, inv, 
@@ -63,11 +64,14 @@ make_helper(exec) {
 		// trap_handler(pc);   //处理异常
 		return;
 	}
+
+
 	instr = instr_fetch(pc, 4);
 	ops_decoded.opcode = instr >> 26;
 	opcode_table[ ops_decoded.opcode ](pc);
 
 	/*----------Gloden_trace----------*/
+	if(temu_state != END){
 	int Rd_num = op_dest->reg;
 	//根据测试更新
 	if(Rd_num==-1)
@@ -82,7 +86,7 @@ make_helper(exec) {
 	// Trace(cpu.pc, Rd_num,regfile[Rd_num], cpu.gpr[Rd_num]._32);
 	// Trace(cpu.pc, Rd_num,regfile[Rd_num], cpu.gpr[Rd_num]._32);
 	/*-------------------------------*/
-	
+	}
 }
 
 static make_helper(_2byte_esc) {
